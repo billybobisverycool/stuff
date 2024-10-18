6e29743aa82fd5fe6ea675cef7870037c2950a7f
billybobisverycool <27johnballenger@ucsstudent.org>
2024-10-18 10:18:11 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,1 @@
-function startGame() {
-    alert("Starting Singleplayer...");
-    // Implement game starting logic here
-}
 
-function loadMultiplayer() {
-    alert("Loading Multiplayer...");
-    // Implement multiplayer loading logic here
-}
-
-function openSettings() {
-    alert("Opening Settings...");
-    // Implement settings logic here
-}
-
-function exitGame() {
-    alert("Exiting Game...");
-    // Implement exit logic here
-}
